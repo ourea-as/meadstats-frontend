2e6e38f4d5bfdb5511e557d7b4be0f3f57168fdb
Fredrik Bore <fredrik@bore.ai>
2020-03-13 20:11:57 +0100

    Remove last remnants of redux

---
+++ src/components/maps/mockmap.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import 'react-flag-icon-css';
+
+import HoverMap from './hovermap';
+import { Loading } from '../loading';
+
+import { API_ROOT } from '../../api/api-config';
+
+type MockMapProps = {
+  username: string;
+};
+
+const MockMap: React.FC<MockMapProps> = ({ username }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    if (username !== '') {
+      axios
+        .get(`${API_ROOT}/v1/users/${username}/countries`)
+        .then(({ data }) => {
+          if (data.status === 'success') {
+            const countries = data.data.countries;
+            setCountries(countries);
+          }
+        })
+        .catch(error => {
+          throw error;
+        });
+    }
+  }, [username]);
+
+  if (!countries.length) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <HoverMap countries={countries} interactive={false} region={'World'} />
+    </>
+  );
+};
+
+export default MockMap;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { store, history } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { CookiesProvider } from 'react-cookie';
 
@@ -12,13 +9,11 @@ import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
-    </ConnectedRouter>
-  </Provider>,
+  <CookiesProvider>
+    <Router>
+      <App />
+    </Router>
+  </CookiesProvider>,
   document.getElementById('root'),
 );
 
